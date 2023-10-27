8cb0aab93e33f1a942605c975c81c0f90ccea164
seanhillweb <seanhillweb@gmail.com>
2023-10-26 19:45:33 -0500

    feat: add frontend mentor assets and scaffolding

---
+++ src/components/attribution.js
@@ -0,0 +1,29 @@
+/**
+ * Summary. Attribution for Frontend Mentor and Sean Hill.
+ */
+
+export default function Attribution() {
+  return (
+    <footer
+      className="text-brand-light-cyan px-4 py-3 text-center text-sm"
+      aria-label="Site"
+    >
+      Challenge by{" "}
+      <a
+        href="https://www.frontendmentor.io?ref=challenge"
+        target="_blank"
+        className="text-brand-neon-green underline hover:no-underline focus:no-underline"
+      >
+        Frontend Mentor
+      </a>
+      . Coded by{" "}
+      <a
+        href="https://www.seanhillweb.com"
+        className="text-brand-neon-green underline hover:no-underline focus:no-underline"
+      >
+        Sean Hill
+      </a>
+      .
+    </footer>
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        brand: {
+          "light-cyan": "#cee3e9",
+          "neon-green": "#52ffa8",
+          "grayish-blue": "#4e5d73",
+          "dark-grayish-blue": "#323a49",
+          "dark-blue": "#1f2632",
+        },
+      },
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1.5rem",
+        sm: "2rem",
+        lg: "3rem",
+        xl: "4rem",
+        "2xl": "5rem",
       },
     },
   },
   plugins: [],
-}
+};
