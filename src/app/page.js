8e5a9d173b27b7e91863635fca22e375d62ca8dd
seanhillweb <seanhillweb@gmail.com>
2023-10-28 15:44:38 -0500

    fix: syntax

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Attribution from "@/components/attribution";
 
 export default function Home() {
   return (
-    <main aria-label="content">
+    <main aria-label="Content">
       <div className="container">
         <div className="flex min-h-screen flex-col items-center justify-center">
           <h1 className="sr-only">
