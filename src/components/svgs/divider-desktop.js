5827b3d67bec59f6acffe79a8e9ae2ee7930a996
seanhillweb <seanhillweb@gmail.com>
2023-10-28 15:23:49 -0500

    feat: add svg components, dice, and dividers

---
+++ src/components/svgs/divider-desktop.js
@@ -0,0 +1,13 @@
+import * as React from "react";
+const DividerDesktop = (props) => (
+  <svg xmlns="http://www.w3.org/2000/svg" width={444} height={16} {...props}>
+    <g fillRule="evenodd">
+      <path fill="#4F5D74" d="M0 8h196v1H0zm248 0h196v1H248z" />
+      <g fill="#CEE3E9" transform="translate(212)">
+        <rect width={6} height={16} rx={3} />
+        <rect width={6} height={16} x={14} rx={3} />
+      </g>
+    </g>
+  </svg>
+);
+export default DividerDesktop;
---
+++ src/components/svgs/divider-mobile.js
@@ -0,0 +1,13 @@
+import * as React from "react";
+const DividerMobile = (props) => (
+  <svg xmlns="http://www.w3.org/2000/svg" width={295} height={16} {...props}>
+    <g fillRule="evenodd">
+      <path fill="#4F5D74" d="M0 8h122v1H0zm173 0h122v1H173z" />
+      <g fill="#CEE3E9" transform="translate(138)">
+        <rect width={6} height={16} rx={3} />
+        <rect width={6} height={16} x={14} rx={3} />
+      </g>
+    </g>
+  </svg>
+);
+export default DividerMobile;
