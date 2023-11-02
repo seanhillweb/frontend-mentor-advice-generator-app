ef1a73dbe485168ac0a94a09e2c01d7725523a6d
seanhillweb <seanhillweb@gmail.com>
2023-11-02 10:54:58 -0500

    feat: add aria attributes for better screen reader announcement

--- src/components/block.js
+++ src/components/block.js
@@ -38,46 +38,45 @@ export default function Block() {
 
   return (
     <div className="round relative block w-full max-w-[540px] rounded-2xl bg-brand-dark-grayish-blue px-4 pb-[64px] pt-8 text-center md:px-11 md:pb-[72px] md:pt-10">
-      {loading ? (
-        <p className="text-xs uppercase tracking-[0.3em] text-brand-neon-green">
-          Advice Loading
-        </p>
-      ) : (
-        <p className="text-xs uppercase tracking-[0.3em] text-brand-neon-green">
-          <span>Advice #</span>
-          {isAdvice.id}
-        </p>
-      )}
-      {loading ? (
-        <p
-          className="mt-6 text-2xl leading-snug text-brand-light-cyan md:text-[28px]"
-          aria-live="polite"
-          aria-atomic="true"
-        >
-          <span>&ldquo;</span>
-          Loading...
-          <span>&rdquo;</span>
-        </p>
-      ) : (
-        <p
-          className="mt-6 text-2xl leading-snug text-brand-light-cyan md:text-[28px]"
-          aria-live="polite"
-          aria-atomic="true"
-        >
-          <span>&ldquo;</span>
-          {isAdvice.advice}
-          <span>&rdquo;</span>
-        </p>
-      )}
-
-      <DividerMobile className="mx-auto mt-6 block md:hidden" />
-      <DividerDesktop className="mx-auto mt-10 hidden md:block" />
+      <div aria-live="polite" aria-atomic="true">
+        {loading ? (
+          <p className="text-xs uppercase tracking-[0.3em] text-brand-neon-green">
+            Advice Loading
+          </p>
+        ) : (
+          <p className="text-xs uppercase tracking-[0.3em] text-brand-neon-green">
+            <span>Advice #</span>
+            {isAdvice.id}
+          </p>
+        )}
+        {loading ? (
+          <p className="mt-6 text-2xl leading-snug text-brand-light-cyan md:text-[28px]">
+            <span aria-hidden="true">&ldquo;</span>
+            Loading...
+            <span aria-hidden="true">&rdquo;</span>
+          </p>
+        ) : (
+          <p className="mt-6 text-2xl leading-snug text-brand-light-cyan md:text-[28px]">
+            <span aria-hidden="true">&ldquo;</span>
+            {isAdvice.advice}
+            <span aria-hidden="true">&rdquo;</span>
+          </p>
+        )}
+      </div>
+      <DividerMobile
+        className="mx-auto mt-6 block md:hidden"
+        aria-hidden="true"
+      />
+      <DividerDesktop
+        className="mx-auto mt-10 hidden md:block"
+        aria-hidden="true"
+      />
       <button
         onClick={handleOnClick}
         className="hover:shadow-3xl focus:shadow-3xl absolute -bottom-8 left-0 right-0 ml-auto mr-auto flex h-16 w-16 items-center justify-center rounded-full bg-brand-neon-green hover:shadow-brand-neon-green focus:shadow-brand-neon-green"
       >
         <span className="sr-only">Generate new advice</span>
-        <DiceSvgComponent className="fill-brand-dark-blue" />
+        <DiceSvgComponent className="fill-brand-dark-blue" aria-hidden="true" />
       </button>
     </div>
   );
